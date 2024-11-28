2b11567299f90c3d412c1ce3e54a732726bc3774
meerim <meerimtee@gmail.com>
2024-11-28 16:29:32 +0600

    Set up maven

--- src/Main.java
+++ src/Main.java
@@ -9,8 +9,6 @@ public class Main {
             ProductController productController = new ProductController();
 
             // Add a new product
-            productController.addProduct(2, "Smartphone", "Iphone 16", 1600.00, 15);
-            productController.addProduct(1, "Laptop", "Lenovo", 1500.00, 10);
 
             productController.getProductsByName("Smartphone");
             productController.getProductsByPriceRange(1,1500.00);
