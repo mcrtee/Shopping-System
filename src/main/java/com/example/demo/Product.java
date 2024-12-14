69947713d1eb0be3988629e95c4b17d2a8079284
avoluknami <imankulovaanabel@gmail.com>
2024-12-15 00:08:23 +0600

    Creating user friendly design

--- src/main/java/com/example/demo/Product.java
+++ src/main/java/com/example/demo/Product.java
@@ -6,7 +6,7 @@ public class Product {
     private double price;
     private int stock;
 
-    public Product(int id, String name, double price, int quantity) {
+    public Product(int id, String name, double price, int stock) {
         this.id = id;
         this.name = name;
         this.price = price;
